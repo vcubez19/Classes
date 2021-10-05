e354c1dfc3e04b662cf544669d80de2c65aacebd
Vincent Cubit <vinniecubit@yahoo.com>
2021-10-04 21:32:05 -0500

    Updated

--- src/main/kotlin/Employee.kt
+++ src/main/kotlin/Employee.kt
@@ -4,6 +4,7 @@
 class Employee(private var empName: String, private var empNumber: String, private var empShift: Int ) {
 
 
+    // Prints each property of the Employee class instance
     fun getAll() {
         println("Name: $empName\nNumber: $empNumber\nShift number: $empShift")
     }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,10 +1,12 @@
 fun main() {
 
+    // Filling out constructor for each employee
     val employeeOne = Employee("Vincent", "111-111-1111", 1)
     val employeeTwo = Employee("Billy", "111-111-2222", 2)
     val employeeThree = Employee("Joe", "111-111-3333", 3)
 
 
+    // Using get all() method to display all info
     employeeOne.getAll()
     println("\n")
     employeeTwo.getAll()
